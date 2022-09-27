79c7b48978608d392436ee27082d010a849e8997
zui0202 <zui0202@naver.com>
2022-09-27 23:01:09 +0900

    make folder structure

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+require('dotenv').config();
+
+const app = express();
+
+app.use(express.json());
+
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+  })
+  .then(() => console.log('Connected to mongodb'))
+  .catch(() => console.log('mongodb connection failed'));
+
+app.listen(8000, () => {
+  console.log(`Server listening on port ${8000}`);
+});
