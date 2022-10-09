1ed7fc8f36528deaba750da4df7ee4ee03278c25
taeju-moon <pp2lycee@naver.com>
2022-10-10 08:10:55 +0900

    commit to pull

--- models/Post.js
+++ models/Post.js
@@ -2,10 +2,9 @@ const mongoose = require('mongoose');
 
 // schema
 const postSchema = mongoose.Schema({
-
   title: { type: String, required: true },
   body: { type: String, required: true },
-  createdAt: { type: Date, default: Date.now }, 
+  createdAt: { type: Date, default: Date.now },
   updatedAt: { type: Date },
 });
 
