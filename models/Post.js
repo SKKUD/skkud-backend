29f0fdb6447052c4a86b6520f23ad833b6d7006e
taeju-moon <pp2lycee@naver.com>
2022-11-11 15:00:15 +0900

    language

--- models/Post.js
+++ models/Post.js
@@ -5,6 +5,10 @@ const postSchema = mongoose.Schema({
   title: { type: String, required: true },
   body: { type: String, required: true },
   tags: [{ type: String }],
+  language: {
+    type: String,
+    required: true,
+  },
   author: { type: mongoose.Schema.Types.ObjectId, ref: 'user' },
   createdAt: { type: Date, default: Date.now },
   updatedAt: { type: Date },
